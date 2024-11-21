b453c4e804c01709ccc787ed532b63dd65468978
BriliyanluthfiTriandri <briliyanlt@gmail.com>
2024-11-21 21:39:10 +0700

    membuat fungsi PengelolaHalaman

--- app/src/main/java/com/example/navigationwithmvvm/PengelolaHalaman.kt
+++ app/src/main/java/com/example/navigationwithmvvm/PengelolaHalaman.kt
@@ -13,9 +13,12 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.example.navigationwithmvvm.ui.viewmodel.MahasiswaViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHost
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.example.navigationwithmvvm.model.DataKelamin
+import com.example.navigationwithmvvm.model.DataMahasiswa
+import com.example.navigationwithmvvm.ui.view.DetailMahasiswaView
 import com.example.navigationwithmvvm.ui.view.FormMahasiswaView
 
 enum class Halaman{
@@ -25,33 +28,7 @@ enum class Halaman{
 
 @Composable
 fun PengelolaHalaman (
-    navController: NavHostController = rememberNavController(),
-    viewModel: MahasiswaViewModel = viewModel()
-) {
-    val uiState by viewModel.uiState.collectAsState()
-
-    NavHost(navController = navController, startDestination = Halaman.Formulir.name) {
-        composable(route = Halaman.Formulir.name) {
-            val konteks = LocalContext.current
-            FormMahasiswaView(
-                listJK = DataKelamin.listJK.map { id ->
-                    konteks.resources.getString(id)
-                },
-                onSubmitClicked = {
-                    viewModel.saveDataMahasiswa(it)
-                    navHost.navigate(Halaman.Detail.name)
-                }
-            )
-        }
-        composable(route = Halaman.Detail.name) {
-            FormMahasiswaView(
-                mhs = uiState,
-                onSubmitClicked = {
-                    navHost.popBackStack()
-                }
-            )
-        }
-
-        }
-    }
-}
+    modifier: Modifier = Modifier,
+    viewModel: MahasiswaViewModel = viewModel(),
+    navHost: NavHostController = rememberNavController()
+) {}
