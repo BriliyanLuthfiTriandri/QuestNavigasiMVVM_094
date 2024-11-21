edc955e0cf43b52215064a0e36d34c207e922f57
BriliyanluthfiTriandri <briliyanlt@gmail.com>
2024-11-21 20:40:04 +0700

    membuat data class DataMahasiswa dengan parameter

--- app/src/main/java/com/example/navigationwithmvvm/model/DataMahasiswa.kt
+++ app/src/main/java/com/example/navigationwithmvvm/model/DataMahasiswa.kt
@@ -1,8 +1,10 @@
 package com.example.navigationwithmvvm.model
 
+
 data class DataMahasiswa(
     val nama: String = "" ,
     val gender: String = "",
     val alamat: String = "",
     val nim: String = " ",
 )
+
